81fc6a61d5c1ccb3da01443eacc043d3ff7967d0
sam <samsulma828@gmail.com>
2018-10-25 15:00:16 +0700

    seeding dan update table store agar expiredate bisa diubah

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Store extends Model
 {
   use SoftDeletes;
-  protected $dates = ['deleted_at'];
+  protected $dates = ['deleted_at', 'expire_date'];
 
   // fungsi untuk mengecek apakakah user memiliki store
   public function owner()
--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -17,7 +17,7 @@ class ItemsTableSeeder extends Seeder
       for ($i=0; $i < 13; $i++) {
         DB::table('items')->insert([
             'name' => $faker->name,
-            'store_id' => 1,
+            'store_id' => 2,
             'price' => $faker->randomNumber,
         ]);
       }
