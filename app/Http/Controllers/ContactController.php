64a4d17a8f1c2d6f07fbd57c80c656a60b8d94d7
sam <samsulma828@gmail.com>
2018-10-25 16:09:15 +0700

    menambahkan middleware gate agar user membuat store terlebih dahulu

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
 
   public function __construct()
   {
-      $this->middleware(['auth', 'get.subscription', 'max.contact']);
+      $this->middleware(['auth', 'gate', 'get.subscription', 'max.contact']);
   }
 
     public function index()
--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -14,7 +14,7 @@ class ItemController extends Controller
 
   public function __construct()
   {
-      $this->middleware(['auth', 'get.subscription']);
+      $this->middleware(['auth', 'gate', 'get.subscription']);
   }
 
   public function index()
