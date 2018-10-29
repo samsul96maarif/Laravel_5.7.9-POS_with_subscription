7c352612c5fe874d32870f3c4bb9e3e4b860e79e
sam <samsulma828@gmail.com>
2018-10-29 07:11:09 +0700

    migrati item media untuk menyimoan gambar

---
+++ database/migrations/2018_10_28_235607_create_item_medias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemMediasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_medias', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('item_id')->unsigned();
+          $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+          $table->string('image');
+          $table->boolean('is_main')->defalut(1);
+          $table->timestamps();
+          $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_medias');
+    }
+}
