4868f1d635bca8ace071b5b2130f5a267952238b
sam <samsulma828@gmail.com>
2018-10-31 21:29:32 +0700

    perbaiki tampilan subscription detail user

--- resources/views/user/subscription/detail.blade.php
+++ resources/views/user/subscription/detail.blade.php
@@ -6,25 +6,27 @@
 
 @section('content')
 
-  <table>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Number invoices</th>
-    <th>Number users</th>
-    <th>Action</th>
-    <tr>
-        <td>{{ $subscription->name }}</td>
-        <td>{{ $subscription->price }}</td>
-        <td>{{ $subscription->num_invoices }}</td>
-        <td>{{ $subscription->num_users }}</td>
-        <td>
-        </td>
-    </tr>
-  </table>
-  <form class="" action="/subscription/beli" method="post">
-    <input type="hidden" name="id" value="{{ $subscription->id }}">
-    {{ csrf_field() }}
-    <input type="submit" name="submit" value="beli">
-  </form>
+  <div class="container">
+    <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">{{ $subscription->name }}</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">Rp.{{ $subscription->price }} <small class="text-muted">/ month</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>free space for items</li>
+              <li>store {{ $subscription->num_invoices }} invoice</li>
+              <li>store {{ $subscription->num_users }} contact</li>
+            </ul>
+            <form  action="/subscription/beli" method="post">
+              <input type="hidden" name="id" value="{{ $subscription->id }}">
+              {{ csrf_field() }}
+              <input class="btn btn-lg btn-block btn-outline-primary" type="submit" name="submit" value="beli">
+            </form>
+          </div>
+        </div>
+    </div>
+  </div>
 
 @endsection
